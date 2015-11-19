5e54f688fe6f4f189103079e562e3543066b17ca
Jean-Richard Lai <jrichardlai@gmail.com>
2015-11-19 09:08:14 -0800

    Add specs

---
+++ examples/rails-4.2.5/spec/requests/posts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", :type => :request do
+  describe "GET /posts" do
+    it "renders posts" do
+      recording_api_interaction do
+        get posts_path
+        expect(response.status).to be(200)
+      end
+    end
+
+    it "renders posts with filter" do
+      recording_api_interaction do |options|
+        options.key = 'with-filter'
+        get posts_path(published: true)
+        expect(response.status).to be(200)
+      end
+    end
+  end
+
+  describe "GET /posts/:id" do
+    it "renders posts with id" do
+      recording_api_interaction do |options|
+        options.path = 'posts/@id'
+        get post_path(Post.first)
+        expect(response.status).to be(200)
+      end
+    end
+  end
+end
---
+++ examples/rails-4.2.5/spec/requests/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Users", :type => :request do
+  describe "GET /users" do
+    it "renders users" do
+      recording_api_interaction do
+        get users_path
+        expect(response.status).to be(200)
+      end
+    end
+  end
+
+  describe "GET /users/:user_id/posts" do
+    it "renders posts" do
+      recording_api_interaction do |options|
+        options.path = 'users/@id/posts'
+        get user_posts_path(User.first)
+        expect(response.status).to be(200)
+      end
+    end
+  end
+end
